c7201fc3900aae8ff0726a01b507bc0ac5969199
Hayden Eastwell <heastwell@tier2consulting.com>
2023-08-01 16:55:13 +0100

    Initial commit

---
+++ src/main/kotlin/haydende/sfgpetclinickotlin/SfgPetClinicKotlinApplication.kt
@@ -0,0 +1,11 @@
+package haydende.sfgpetclinickotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SfgPetClinicKotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<SfgPetClinicKotlinApplication>(*args)
+}
